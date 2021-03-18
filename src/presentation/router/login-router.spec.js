6c6cc9f7d75306a9cd289601c1b46d5d7219da6e
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-17 21:38:30 -0300

    ensore LoginRouter retuns 500 if an invalid httpRequest is provider

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -1,6 +1,12 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+    const { email, passoword } = httpRequest.body
+    if (!email || !passoword) {
       return {
         statusCode: 400
       }
@@ -9,7 +15,7 @@ class LoginRouter {
 }
 
 describe('Login Router', () => {
-  test('Should return 400 if no emial is provaider', () => {
+  test('Should return 400 if no password is provider', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -20,7 +26,7 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(400)
   })
 
-  test('Should return 400 if no emial is provaider', () => {
+  test('Should return 400 if no email is provider', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -29,5 +35,17 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provider', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if no httpRequest is body', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
