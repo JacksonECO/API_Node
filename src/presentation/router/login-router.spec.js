41c84bdbfb711cd280a9bcdbcfbc8b40846e3159
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-17 22:06:41 -0300

    ensure LoginRouter return  a Custom error if email and password are provider

--- src/presentation/router/login-router.spec.js
+++ src/presentation/router/login-router.spec.js
@@ -1,21 +1,42 @@
 class LoginRouter {
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return {
-        statusCode: 500
-      }
+      return HttpResponse.serverError()
     }
     const { email, passoword } = httpRequest.body
-    if (!email || !passoword) {
-      return {
-        statusCode: 400
-      }
+    if (!email) {
+      return HttpResponse.badRequest('email')
+    }
+    if (!passoword) {
+      return HttpResponse.badRequest('password')
     }
   }
 }
 
+class HttpResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super('Missing param: ' + paramName)
+    this.name = 'MissingParamError'
+  }
+}
+
 describe('Login Router', () => {
-  test('Should return 400 if no password is provider', () => {
+  test('Should return 400 if no email is provider', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -24,9 +45,10 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('Should return 400 if no email is provider', () => {
+  test('Should return 400 if no password is provider', () => {
     const sut = new LoginRouter()
     const httpRequest = {
       body: {
@@ -35,6 +57,7 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
   test('Should return 500 if no httpRequest is provider', () => {
