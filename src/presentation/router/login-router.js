c806e2312998fff340e522c9e727b173c6524424
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-17 22:51:07 -0300

    esquecido

--- src/presentation/router/login-router.js
+++ src/presentation/router/login-router.js
@@ -17,5 +17,8 @@ module.exports = class LoginRouter {
       return HttpResponse.badRequest('password')
     }
     this.authUseCase.auth(email, passoword)
+    return {
+      statusCode: 401
+    }
   }
 }
