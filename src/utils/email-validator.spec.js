29b3a296630158a9b0befda8a5f731a61654e193
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-18 17:44:44 -0300

    move EmailValidator to its own file

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,10 +1,5 @@
 const validator = require('validator')
-
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
-}
+const EmailValidator = require('./email-validator')
 
 const mekeSut = () => {
   return new EmailValidator()
