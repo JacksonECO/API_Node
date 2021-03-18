3b63653383361d34d225ac3681fe09a2808a2b88
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-18 17:27:39 -0300

    move sut creation to a factory helper method

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,16 +6,20 @@ class EmailValidator {
   }
 }
 
+const mekeSut = () => {
+  return new EmailValidator()
+}
+
 describe('Email Validator', () => {
   test('Shold return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = mekeSut()
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Shold return false if validator returns false', () => {
     validator.isValid = false
-    const sut = new EmailValidator()
+    const sut = mekeSut()
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
