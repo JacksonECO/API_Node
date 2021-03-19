e73f7781afe964c2611297d90df0f2c9195df9b0
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-18 21:58:45 -0300

    move sut creation to a factory helpers method

--- src/domain/usecases/auth-usercase.spec.js
+++ src/domain/usecases/auth-usercase.spec.js
@@ -1,8 +1,8 @@
 const { MissingParamError } = require('../../utils/erros/index')
 
 class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
+  constructor (loadUserByEmailRepositorySpy) {
+    this.loadUserByEmailRepositorySpy = loadUserByEmailRepositorySpy
   }
 
   async auth (email, password) {
@@ -12,32 +12,37 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
-    await this.loadUserByEmailRepository.load(email)
+    await this.loadUserByEmailRepositorySpy.load(email)
   }
+}
+
+const makeSut = () => {
+  class LoadUserByEmailRepository {
+    async load (email) {
+      this.email = email
+    }
+  }
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepository()
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+  return { sut, loadUserByEmailRepositorySpy }
 }
 
 describe('Auth UseCase', () => {
   test('Should throw if no email is provider', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('Should throw if no password is provider', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('ant_email@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   test('Should call LoadUserByEmailRepository with correct email', async () => {
-    class LoadUserByEmailRepository {
-      async load (email) {
-        this.email = email
-      }
-    }
-    const loadUserByEmailRepositoru = new LoadUserByEmailRepository()
-    const sut = new AuthUseCase(loadUserByEmailRepositoru)
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
     await sut.auth('ant_email@mail.com', 'any_passowrd')
-    expect(loadUserByEmailRepositoru.email).toBe('ant_email@mail.com')
+    expect(loadUserByEmailRepositorySpy.email).toBe('ant_email@mail.com')
   })
 })
