1309d24d4dd9b5084d0427f909ea723c4808b1d9
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-20 11:43:02 -0300

    move sur creation to a factory helpers method

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -2,29 +2,31 @@ const bcrypt = require('bcrypt')
 
 class Encrypter {
   async compare (value, hash) {
-    this.value = value
-    this.hash = hash
     const isValid = await bcrypt.compare(value, hash)
     return isValid
   }
 }
 
+const makeSut = () => {
+  return new Encrypter()
+}
+
 describe('Encrypter', () => {
   test('Should return true is bcrypt return true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('Should return true is bcrypt return true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
   test('Should call bcrypt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
