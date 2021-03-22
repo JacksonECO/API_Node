b4f8d0649e0d36d97926f787d557572f9632353a
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-22 14:04:31 -0300

    ensure requests parse body as JSON

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,8 +1,10 @@
 const cors = require('../middlewares/cors')
+const jsonParse = require('../middlewares/json-parser')
 
 module.exports = app => {
   app.disable('x-powered-by')
-  // Permitir que a nossa api receba requisições de outros domínios
-  app.use(cors)
-  // app.use()
+
+  app.use(cors)// Permitir que a nossa api receba requisições de outros domínios
+
+  app.use(jsonParse)
 }
