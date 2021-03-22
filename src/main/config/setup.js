8a369b5b62b1a96d7a50596f6ffc2b643b6389ae
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-22 13:19:04 -0300

    move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,10 +1,7 @@
+const cors = require('../middlewares/cors')
+
 module.exports = app => {
   app.disable('x-powered-by')
   // Permitir que a nossa api receba requisições de outros domínios
-  app.use((req, res, next) => {
-    res.set('access-control-allow-origin', '*')
-    res.set('access-control-allow-methods', '*')
-    res.set('access-control-allow-headers', '*')
-    next()
-  })
+  app.use(cors)
 }
