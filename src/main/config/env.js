a64b8064125472b9411864b142b234daf8b4129d
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-23 10:44:14 -0300

    fix mongodb port and app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:2701/API_Node'
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:xxx/API_Node',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret'
-  // port: process.env.PORT || 5858
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/API_Node',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.PORT || 5858
 }
--- src/main/index.js
+++ src/main/index.js
@@ -4,6 +4,6 @@ const env = require('./config/env')
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
-    app.listen(5858, () => console.log('Server Runing'))
+    app.listen(env.port, () => console.log(`Server runing at http://localhost:${env.port}`))
   })
   .catch(console.log)
