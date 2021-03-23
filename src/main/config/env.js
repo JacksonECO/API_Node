ae7ed12a0e932ddd55b2fc3f2104d248b4c6915a
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-23 00:36:52 -0300

    add routes file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,6 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:2701/API_Node'
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:2701/API_Node'
+  mongoUrl: process.env.MONGO_URL || 'http://localhost:27017'
+  // tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  // port: process.env.PORT || 5858
 }
